1c8496dd4f574e443da488a26598f1f74551253e
Grandma Goblin <133256666+grandmagoblin@users.noreply.github.com>
2023-06-12 22:40:08 -0400

    Adding make files for atom xl

---
+++ lineage_gargoyle_atom_xl_bgN.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, device/phh/treble/lineage_arm64_bgN.mk)
+$(call inherit-product, device/unihertz/lineage_gargoyle.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE = gargoyle
+PRODUCT_BRAND = Unihertz
+PRODUCT_SYSTEM_BRAND = Unihertz
+PRODUCT_MANUFACTURER = Unihertz
+PRODUCT_NAME = Atom XL
+PRODUCT_MODEL = Atom XL
+
