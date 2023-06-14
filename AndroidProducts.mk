c461af7d4c55e665803ae2a150ec1c3366464875
Grandma Goblin <133256666+grandmagoblin@users.noreply.github.com>
2023-06-13 20:43:45 -0400

    Update AndroidProducts.mk
    Include atom xl and atom l

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,6 +7,10 @@ PRODUCT_MAKEFILES += \
 	$(LOCAL_DIR)/lineage_gargoyle_pocket_bgN.mk \
 	$(LOCAL_DIR)/lineage_gargoyle_tank_bvN.mk \
 	$(LOCAL_DIR)/lineage_gargoyle_tank_bgN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_atom_l_bvN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_atom_l_bgN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_atom_xl_bvN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_atom_xl_bgN.mk \
 
 COMMON_LUNCH_CHOICES := \
 	lineage_gargoyle_bvN-userdebug \
@@ -17,3 +21,7 @@ COMMON_LUNCH_CHOICES := \
 	lineage_gargoyle_pocket_bgN-userdebug \
 	lineage_gargoyle_tank_bvN-userdebug \
 	lineage_gargoyle_tank_bgN-userdebug \
+	lineage_gargoyle_atom_l_bvN-userdebug \
+	lineage_gargoyle_atom_l_bgN-userdebug \
+	lineage_gargoyle_atom_xl_bvN-userdebug \
+	lineage_gargoyle_atom_xl_bgN-userdebug \
